14550df6e95e7bf11761e99f1e667180c2bc26b2
melissaprax <melissaprax@gmail.com>
2018-05-13 19:00:35 -0500

    arrays of pets and their scores

---
+++ app/data/pets.js
@@ -0,0 +1,76 @@
+
+let puppy = {  "name":"puppy",
+    "photo":"./images/puppy.png",
+    "scores":[
+        0,
+        5,
+        0,
+        5,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+      ]
+  }
+  let kitten = {  "name":"kitten",
+    "photo":"./images/kitten.jpg",
+    "scores":[
+        0,
+        0,
+        5,
+        0,
+        0,
+        0,
+        5,
+        0,
+        0,
+        0
+      ]
+  }
+  let rock = {  "name":"rock",
+    "photo":"./images/petrock.jpg",
+    "scores":[
+        0,
+        0,
+        0,
+        0,
+        5,
+        0,
+        0,
+        0,
+        0,
+        5
+      ]
+  }
+  let parakeet = {  "name":"parakeet",
+    "photo":"./images/parakeet.jpeg",
+    "scores":[
+        5,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        5,
+        0,
+        0
+      ]
+  }
+  let fish = {  "name":"fish",
+    "photo":"./images/fish.jpg",
+    "scores":[
+        0,
+        0,
+        0,
+        0,
+        0,
+        5,
+        0,
+        0,
+        5,
+        0
+      ]
+  }
